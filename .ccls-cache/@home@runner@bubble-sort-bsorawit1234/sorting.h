54e0b4b7cb2f911d2743e6c499300e80b72a5510
bsorawit1234 <100036251+bsorawit1234@users.noreply.github.com>
2023-01-27 08:52:42 +0000

    update bubble

--- .ccls-cache/@home@runner@bubble-sort-bsorawit1234/sorting.h
+++ .ccls-cache/@home@runner@bubble-sort-bsorawit1234/sorting.h
@@ -28,12 +28,12 @@ void bubbleSort(int a[], int n) {
   int i, j, t;
   int sorted;
   // how may pair to compare?
-  for (i = 0; i < n - 1; i++) {
-    for (j = 0; j < n - 1; j++) {
+  for (i = n - 1; i > 0; i--) {
+    for (j = 0; j < i; j++) {
       if (a[j] > a[j + 1]) {
         swap(&a[j], &a[j + 1]);
       }
+      // display(a, n);
     }
-    display(a, n);
   }
 }
